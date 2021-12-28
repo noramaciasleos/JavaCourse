46ab2a0e9537ec873f049130507d2b0955c7746e
Nora Macias Leos <nora.macias@globant.com>
2021-12-28 11:42:25 -0600

    JAva training

---
+++ Java2/Puzzle4.java
@@ -0,0 +1,16 @@
+public class Puzzle4 {
+    public static void   main (String [] args){
+        Puzzle4b[] obs= new Puzzle4b[6];
+        int y=1;
+        int x=0;
+        while (x<6){
+            obs[x]=new Puzzle4b();
+
+        }
+        x = 6;
+        while (x > 0) {
+            _________________
+                    result = result + ___________________
+        }
+    }
+}
---
+++ Java2/Puzzle4b.java
@@ -0,0 +1,12 @@
+public class Puzzle4b {
+    int ivar;
+        doStuff (int){
+            if (ivar>100){
+                return
+
+            } else {
+                return;
+            }
+
+        }
+}
